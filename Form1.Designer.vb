95a3439b94942586bcc01f7edcea5d45c05c1427
Seb <sebastien@yoctopuce.com>
2018-01-03 20:03:37 +0100

    fist commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.components = New System.ComponentModel.Container()
+    Me.Label1 = New System.Windows.Forms.Label()
+    Me.Label2 = New System.Windows.Forms.Label()
+    Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+    Me.SuspendLayout
+    '
+    'Label1
+    '
+    Me.Label1.AutoSize = true
+    Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+    Me.Label1.Location = New System.Drawing.Point(13, 13)
+    Me.Label1.Name = "Label1"
+    Me.Label1.RightToLeft = System.Windows.Forms.RightToLeft.No
+    Me.Label1.Size = New System.Drawing.Size(51, 17)
+    Me.Label1.TabIndex = 0
+    Me.Label1.Text = "Label1"
+    '
+    'Label2
+    '
+    Me.Label2.AutoSize = true
+    Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 32!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+    Me.Label2.Location = New System.Drawing.Point(9, 50)
+    Me.Label2.Name = "Label2"
+    Me.Label2.Size = New System.Drawing.Size(152, 51)
+    Me.Label2.TabIndex = 1
+    Me.Label2.Text = "Label2"
+    '
+    'Timer1
+    '
+    Me.Timer1.Enabled = true
+    '
+    'Form1
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(284, 261)
+    Me.Controls.Add(Me.Label2)
+    Me.Controls.Add(Me.Label1)
+    Me.Name = "Form1"
+    Me.Text = "Form1"
+    Me.ResumeLayout(false)
+    Me.PerformLayout
+
+End Sub
+
+  Friend WithEvents Label1 As Label
+  Friend WithEvents Label2 As Label
+  Friend WithEvents Timer1 As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+  Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Dim errmsg as String = ""
+    If YAPI.RegisterHub("usb", errmsg) <> YAPI_SUCCESS Then
+      MessageBox.Show("Init error:" + errmsg)
+      Application.Exit()
+    End If
+    label1.Text = "No sensor detected"
+    Label2.Text = "N/A"
+    Timer1.Enabled = True
+  End Sub
+
+  Dim sensor As YSensor = Nothing
+  dim hardwaredetect As Integer = 0
+
+  Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+    Dim errmsg As String = ""
+
+    If hardwaredetect = 0 Then
+      YAPI.UpdateDeviceList(errmsg)
+    End If
+    hardwaredetect = (hardwaredetect + 1) Mod 20
+    YAPI.HandleEvents(errmsg)
+    If sensor Is Nothing Then
+      sensor = YSensor.FirstSensor()
+    End If
+    If Not sensor Is Nothing Then
+      If sensor.isOnline() Then
+        label1.Text = sensor.get_friendlyName()
+        label2.Text = Str(sensor.get_currentValue()) + sensor.get_unit()
+      Else
+        label1.Text = "Sensor is offline"
+        label2.Text = "OFFLINE"
+        sensor = Nothing
+      End If
+    End If
+  End Sub
+End Class
